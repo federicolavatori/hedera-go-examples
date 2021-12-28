cf1870cee5943ee0808b773c6791ce84e264b3dc
federico.lavatori@gmail.com <federico.lavatori@gmail.com>
2021-12-29 00:02:07 +0100

    created testnet client

--- hedera_examples.go
+++ hedera_examples.go
@@ -4,8 +4,8 @@ import (
     "fmt"
     "os"
 
-    "github.com/joho/godotenv"
     "github.com/hashgraph/hedera-sdk-go/v2"
+    "github.com/joho/godotenv"
 )
 
 func main() {
@@ -16,7 +16,7 @@ func main() {
         panic(fmt.Errorf("Unable to load environment variables from .env file. Error:\n%v\n", err))
     }
 
-  //Grab your testnet account ID and private key from the .env file
+    //Grab your testnet account ID and private key from the .env file
     myAccountId, err := hedera.AccountIDFromString(os.Getenv("MY_ACCOUNT_ID"))
     if err != nil {
         panic(err)
@@ -30,4 +30,8 @@ func main() {
     //Print your testnet account ID and private key to the console to make sure there was no error
     fmt.Printf("The account ID is = %v\n", myAccountId)
     fmt.Printf("The private key is = %v\n", myPrivateKey)
+
+    //Create your testnet client
+    client := hedera.ClientForTestnet()
+    client.SetOperator(myAccountId, myPrivateKey)
 }

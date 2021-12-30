408e6c9960e57eae5970d2621b8a0053125c42e9
federico.lavatori@gmail.com <federico.lavatori@gmail.com>
2021-12-30 21:17:18 +0100

    create account with balance and print it

--- hedera_examples.go
+++ hedera_examples.go
@@ -34,4 +34,43 @@ func main() {
     //Create your testnet client
     client := hedera.ClientForTestnet()
     client.SetOperator(myAccountId, myPrivateKey)
+
+    //Generate new keys for the account you will create
+    newAccountPrivateKey, err := hedera.GeneratePrivateKey()
+    if err != nil {
+        panic(err)
+    }
+
+    newAccountPublicKey := newAccountPrivateKey.PublicKey()
+
+    //Create new account and assign the public key
+    newAccount, err := hedera.NewAccountCreateTransaction().
+        SetKey(newAccountPublicKey).
+        SetInitialBalance(hedera.HbarFrom(1000, hedera.HbarUnits.Tinybar)).
+        Execute(client)
+
+    //Request the receipt of the transaction
+    receipt, err := newAccount.GetReceipt(client)
+    if err != nil {
+        panic(err)
+    }
+
+    //Get the new account ID from the receipt
+    newAccountId := *receipt.AccountID
+
+    //Print the new account ID to the console
+    fmt.Printf("The new account ID is %v\n", newAccountId)
+
+    //Create the account balance query
+    query := hedera.NewAccountBalanceQuery().
+        SetAccountID(newAccountId)
+
+    //Sign with client operator private key and submit the query to a Hedera network
+    accountBalance, err := query.Execute(client)
+    if err != nil {
+        panic(err)
+    }
+
+    //Print the balance of tinybars
+    fmt.Println("The account balance for the new account is ", accountBalance.Hbars.AsTinybar())
 }
